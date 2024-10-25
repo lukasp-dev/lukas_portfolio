7dda300f682a3fcfde73588fc240a51010e72eb0
lukasp-dev <nubddak3@gmail.com>
2024-10-25 03:22:34 -0400

    type error fixed

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -84,7 +84,6 @@ const About = () => {
                                 height={326}
                                 width={326}
                                 backgroundColor="rgba(0, 0, 0, 0)"
-                                backgroundImageOpacity={0.5}
                                 showAtmosphere
                                 showGraticules
                                 globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
