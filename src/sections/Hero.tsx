4dc427ebb9c5575435135d345bba86864e46f0c3
lukasp-dev <nubddak3@gmail.com>
2025-06-24 23:01:32 -0400

    resume change

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -53,7 +53,7 @@ const Hero = () => {
             Navigate through my portfolio
           </button>
           <a
-            href="https://drive.google.com/file/d/1dpCYsKL0N9rkLim5Lz2KUyINpwJrjTRr/view?usp=sharing"
+            href="https://drive.google.com/file/d/15x0BdPfPAEVfnd2kY4kgFh-9PSy9nJ3c/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="ml-2 mt-4 px-4 py-3 text-white rounded transition bg-blue-800 hover:bg-blue-900"
